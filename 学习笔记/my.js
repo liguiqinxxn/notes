91f78bcbfdeda2473f1637057cf790166e9a3615
lgq186 <1043736808@qq.com>
2018-08-30 14:35:44 +0800

    TweenMax

--- 学习笔记/my.js
+++ 学习笔记/my.js
@@ -232,7 +232,7 @@ function drag(obj) {
 function setCookie(key, value, t) {
 	var oDate = new Date();
 	oDate.setDate( oDate.getDate() + t );
-	document.cookie = key + '=' + value + ';expires=' + oDate.toGMTString();
+	document.cookie = key + '=' + value + ';expires=' + oDate.toUTCString();
 }
 // 获取cookie
 function getCookie(key) {
