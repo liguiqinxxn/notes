e37890a89450d61bba03d680f6303530f59dc6e8
lgq186 <1043736808@qq.com>
2018-11-12 18:43:35 +0800

    DOM优化、快速排序

--- 学习笔记/my.js
+++ 学习笔记/my.js
@@ -249,3 +249,24 @@ function removeCookie(key) {
 	setCookie(key, '', -1);
 }
 
+// 快速排序（从小到大）
+function quickSort(){
+	if(arr.length <= 1){
+		return arr;
+	}
+
+	var num = Math.floor(arr.length/2);
+	var numValue = arr.splice(num,1);
+
+	var left = [];
+	var right = [];
+
+	for(var i=0;i<arr.length;i++){
+		if(arr[i]<numValue){
+			left.push(arr[i]);
+		}else{
+			right.push(arr[i]);
+		}
+	}
+	return quickSort(left).concat([numValue],quickSort(right));
+}
